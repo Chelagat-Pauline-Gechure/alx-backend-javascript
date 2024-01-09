3042c5a4a9fea58e45a8001eafb59b148e311108
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-01-09 18:37:34 +0000

    Task 2 - 5

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsByCity = students.filter((std) => std.location === city);
+
+  return studentsByCity.map((std) => {
+    const stdGrade = newGrades.find((grade) => grade.studentId === std.id);
+    return {
+      ...std,
+      grade: stdGrade ? stdGrade.grade : 'N/A',
+
+    };
+  });
+}
